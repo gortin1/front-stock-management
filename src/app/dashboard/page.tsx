7a5b928068e41480e96ab4575bdabd3fc66ab4f7
gortin <bielsanfru@gmail.com>
2025-11-12 09:37:49 -0300

    fix: table head

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -228,6 +228,9 @@ const DashboardPage: React.FC = () => {
                 <thead className="bg-gray-50">
                   <tr>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Imagem
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Produto
                     </th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
